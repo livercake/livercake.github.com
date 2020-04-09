a1fdc34eab50096a9d4665712665ca08e5ac0352
Felipe Parada <fparada@digitalmeat.cl>
2020-04-08 22:42:00 -0400

    vuejs inlined

---
+++ _site/js/app.js
@@ -0,0 +1,21 @@
+// JS-triggered resource init
+// TODO: bitchin' loading sequence?
+window.addEventListener('load', (event) => {
+  console.info('assets loaded. welcome to the liverblog! 🥰');
+  console.log(event);
+});
+
+
+// Inline vue init
+var app = new Vue({
+  el: '#app',
+  // Local component declaration
+  // dependson httpVueLoader
+  components: {
+    'Colophon': httpVueLoader('./js/components/Colophon.vue'),
+  },
+  data: {
+    // sample vue data
+    message: 'Welcome to the liverblog!'
+  }
+})
---
+++ _site/js/components/Colophon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="index-colophon">
+    <img class="index-colophon_avatar" :src="this.$attrs.avatar" alt="this is liver">
+    <div class="index-colophon_description">
+      <small>livercake es</small>
+      <h2>{{this.$attrs.bio}}</h2>
+      <p>{{this.$attrs.description}}</p>
+    </div>
+    <!-- <button class="index-colophon_cta">
+      Autobombo / Selfpromotion &nbsp; <i class="fa fa-angle-down"></i>
+    </button> -->
+  </div>
+</template>
