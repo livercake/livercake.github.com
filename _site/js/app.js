e92ccb53087d2d5ffc11f4605ee263c2ed69f0c9
Felipe Parada <fparada@digitalmeat.cl>
2020-04-13 22:29:57 -0400

    test cors

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,11 +1,3 @@
-// JS-triggered resource init
-// TODO: bitchin' loading sequence?
-window.addEventListener('load', (event) => {
-  console.info('assets loaded. welcome to the liverblog! 🥰');
-  console.log(event);
-});
-
-
 // Inline vue init
 var app = new Vue({
   el: '#app',
@@ -17,6 +9,40 @@ var app = new Vue({
   },
   data: {
     // sample vue data
-    message: 'Welcome to the liverblog!'
+    message: 'Welcome to the liverblog!',
+    isLoading: true,
+  },
+  methods: {
+    getVideoGame(id) {
+      const API_KEY = '88b3b64891078889ea76b80e9e4027f2';
+      window.axios({
+        url: "https://api-v3.igdb.com/games",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': API_KEY
+        },
+      })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+  },
+  mounted() {
+    // JS-triggered resource init
+    // TODO: bitchin' loading sequence?
+    const vue = this;
+    window.addEventListener('load', (event) => {
+      console.info('assets loaded. welcome to the liverblog! 🥰');
+      console.log(event);
+      window.setTimeout(vue.isLoading = false, 3000);
+
+      vue.getVideoGame();
+
+    });
+
   }
 })
--- _site/js/components/PlayThis.vue
+++ _site/js/components/PlayThis.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="play-this">
-    <h4 class="title">
-      <i class="fa fa-music"></i>
-      <small>dale, liver,</small>
-      tócate un tema!
-    </h4>
-    <iframe
-      :src="`https://open.spotify.com/embed/track/${this.$attrs.embed}`"
-      width="300"
-      height="380"
-      frameborder="0"
-      allowtransparency="true"
-      allow="encrypted-media">
-    </iframe>
-    <div class="disclaimer text-center">
-      <i class="fa fa-warning"></i> si no estás logeado al spotify son sólo 30 segundos 😕
+  <div class="play-this hobbies">
+    <!-- <div class="hobby-loading">
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    </div> -->
+    <div class="hobby-loaded">
+      <h4 class="title">
+        <i class="fa fa-music"></i>
+        <small>dale, liver,</small>
+        tócate un tema!
+      </h4>
+      <iframe
+        :src="`https://open.spotify.com/embed/track/${this.$attrs.embed}`"
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media">
+      </iframe>
+      <div class="disclaimer text-center">
+        <i class="fa fa-warning"></i> si no estás logeado al spotify son sólo 30 segundos 😕
+      </div>
     </div>
   </div>
 </template>
